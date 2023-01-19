afe7a00f798c278c77dd4ce0b0dcbe8ef4c5ec56
Parveen Bhadoo <parveenbhadoo@gmail.com>
2023-01-19 05:27:26 +0000

    Bug Fix

--- worker/worker-super-api.js
+++ worker/worker-super-api.js
@@ -503,13 +503,8 @@ async function handleRequest(request, event) {
     //var loginCheck = await loginHandleRequest(event)
     //if(authConfig['enable_auth0_com'] && loginCheck != null){return loginCheck}
     const region = request.headers.get('cf-ipcountry').toUpperCase();
-    const asn_servers = '';
-    try {
+    var asn_servers = '';
     try {var asn_servers = request.cf.asn;}catch {var asn_servers = 0;}
-    }
-    catch {
-    const asn_servers = 111;
-    }
     const referer = request.headers.get("Referer");
     if (gds.length === 0) {
         for (let i = 0; i < authConfig.roots.length; i++) {
